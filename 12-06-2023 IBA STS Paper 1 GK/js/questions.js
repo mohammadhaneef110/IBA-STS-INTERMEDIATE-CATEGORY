0b8d743c23c3e593a7e232f7b6d7e24aeb988269
webdeveloper110 <143437327+webdeveloper110@users.noreply.github.com>
2023-10-11 13:32:17 +0100

    IBA STS INTERMEDIATE CATEGORY QUZ

---
+++ 12-06-2023 IBA STS Paper 1 GK/js/questions.js
@@ -0,0 +1,184 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The capital of Iran is:",
+    answer: "Tehran",
+    options: [
+      "Kabul",
+      "Tehran",
+      "Zahedan", 
+      "Taftan", 
+    ]
+  },
+    {
+    numb: 2,
+    question: "The Shrine of Khawaja Ghulam Farid(R.H) is located in:",
+    answer: "Rajanpur",
+    options: [
+      "Ranikot",
+      "Rajanpur",
+      "Ranipur",
+      "Rawalkot",
+    ]
+  },
+    {
+    numb: 3,
+    question:  "Arabic is not a official language of:",
+    answer: "Turkey",
+    options: [
+      "Kuwait", 
+      "Qatar", 
+      "Bahrain", 
+      "Turkey",
+      
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who authored 'Long Walk of Freedom':",
+    answer: "Nelson Mandela",
+    options: [
+      "Karl Marks", 
+      "Jawaharial Nehru", 
+      "Nelson Mandela",
+      "Barack Hussein Obama", 
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following landlocked sea?",
+    answer: "Aral Sea",
+    options: [
+      "Arabian sea", 
+      "South China Sea", 
+      "Aral Sea",
+      "Yellow Sea", 
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which of the following languages has most alphabets?",
+    answer: "Sindhi",
+    options: [
+      "Urdu", 
+      "Sindhi",
+      "English", 
+      "Balochi", 
+    ]
+  },
+    {
+    numb: 7,
+    question: "Tibet is an autonomous region of:",
+    answer: "China",
+    options: [
+      "India", 
+      "Russia", 
+      "China",
+      "Nepal", 
+    ]
+  },
+    {
+    numb: 8,
+    question: "Official Currency of Yemen is:",
+    answer: "Rial",
+    options: [
+      "Rial", 
+      "Dirham", 
+      "Dinar",
+      "Ringgit", 
+    ]
+  },
+    {
+    numb: 9,
+    question: "+92 is the country code of:",
+    answer: "Pakistan",
+    options: [
+      "Saudi Arabia", 
+      "China", 
+      "Pakistan",
+      "Kuwait", 
+    ]
+  },
+    {
+    numb: 10,
+    question: "Tomb of Babur the First Mughal emperor is in:",
+    answer: "Kabul",
+    options: [
+      "Tashkent", 
+      "Bukhara", 
+      "Kabul",
+      "Peshawar", 
+    ]
+  },
+    
+    {
+    numb: 11,
+    question: "National Flower of Pakistan is:",
+    answer: "Jasmine",
+    options: [
+      "Rose", 
+      "Tulip", 
+      "Jasmine",
+      "Lily", 
+    ]
+  },
+    {
+    numb: 12,
+    question: "The Secretary General of the OIC is elected for the period of:",
+    answer: "5 Years",
+    options: [
+      "3 Years", 
+      "4 Years", 
+      "5 Years",
+      "6 Years", 
+    ]
+  },
+    {
+    numb: 13,
+    question: "Which of the following uses GMT+5 as its standard time?",
+    answer: "Pakistan",
+    options: [
+      "Bangladesh", 
+      "India", 
+      "Pakistan",
+      "Afghanistan", 
+    ]
+  },
+    {
+    numb: 14,
+    question: "The Land of Kangaroos:",
+    answer: "Australia",
+    options: [
+      "Brazil", 
+      "Australia", 
+      "South Africa",
+      "Thailand", 
+    ]
+  },
+    {
+    numb: 15,
+    question: "Which country won ICC Women's Cricket World Cup 2022",
+    answer: "Australia",
+    options: [
+      "India", 
+      "England", 
+      "Australia",
+      "South Africa", 
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
